208ce368d2380c83a37eba8ccb9de6a69121c868
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-01-16 02:52:45 +0700

    2.0.0

--- page/reference/extension/index.php
+++ page/reference/extension/index.php
@@ -6,21 +6,24 @@ Hook::set('*.content', function($content, $lot) {
     }
     $N = isset($lot['name']) ? $lot['name'] : Path::N($lot['path']);
     $NS = basename(dirname($lot['path'])) === 'plugin' ? 'extend.plugin' : 'extend';
+    $stats = include ROOT . DS . 'stats.php';
+    $i = 'https://github.com/mecha-cms/' . $NS . '.' . $N . '/archive/master.zip';
+    $i = isset($stats[$i]) ? $stats[$i] : 0;
     if (!isset($lot['dependency']) || $lot['dependency'] !== false) {
-        $content = '[Download Latest Version](%{url}%/2016/d/mecha-cms/' . $NS . '.' . $N . '/archive/master.zip) {.button}' . N . N . $content;
+        $content = '[Download Latest Version](%{url}%/r/git:mecha-cms/' . $NS . '.' . $N . '/archive/master.zip "' . $i . ' Downloads") {.button}' . N . N . $content;
     }
     if (!empty($lot['dependency']) && $lot['dependency'] !== true) {
         $content .= N . N . '### Dependency';
         if (!empty($lot['dependency']['extension'])) {
             $content .= N . N . '#### Extension' . N;
             foreach ($lot['dependency']['extension'] as $v) {
-                $content .= N . ' - [link:/reference/extension/' . $v . ']';
+                $content .= N . ' - [link:/reference/extension/' . str_replace('.', '-', $v) . ']';
             }
         }
         if (!empty($lot['dependency']['plugin'])) {
             $content .= N . N . '#### Plugin' . N;
             foreach ($lot['dependency']['plugin'] as $v) {
-                $content .= N . ' - [link:/reference/extension/plugin/' . $v . ']';
+                $content .= N . ' - [link:/reference/extension/plugin/' . str_replace('.', '-', $v) . ']';
             }
         }
     }
@@ -29,13 +32,13 @@ Hook::set('*.content', function($content, $lot) {
         if (!empty($lot['dependency_x']['extension'])) {
             $content .= N . N . '#### Extension' . N;
             foreach ($lot['dependency_x']['extension'] as $v) {
-                $content .= N . ' - [link:/reference/extension/' . $v . ']';
+                $content .= N . ' - [link:/reference/extension/' . str_replace('.', '-', $v) . ']';
             }
         }
         if (!empty($lot['dependency_x']['plugin'])) {
             $content .= N . N . '#### Plugin' . N;
             foreach ($lot['dependency_x']['plugin'] as $v) {
-                $content .= N . ' - [link:/reference/extension/plugin/' . $v . ']';
+                $content .= N . ' - [link:/reference/extension/plugin/' . str_replace('.', '-', $v) . ']';
             }
         }
     }
