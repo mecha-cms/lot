cd99d527c98583c7534ed0ff96e31ae04396e0f0
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-01-18 23:34:29 +0700

    Update

--- shield/$/404.php
+++
@@ -1,1 +0,0 @@
-<?php require GROUND . DS . '404.php';
--- shield/$/pages.php
+++
@@ -1,9 +0,0 @@
-<?php static::header(); ?>
-<?php foreach ($pages as $page): ?>
-<article>
-  <header><h3><a href="<?php echo $page->link ?: $page->url; ?>"><?php echo $page->title; ?></a></h3></header>
-  <div><?php echo $page->description; ?></div>
-</article>
-<?php endforeach; ?>
-<?php static::pager('step'); ?>
-<?php static::footer(); ?>
